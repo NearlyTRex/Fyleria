3e711fb545859f7711b13916edb7d81fcd5045d2
NearlyTRex <evermoresecrets@gmail.com>
2019-05-08 16:41:10 -0400

    Adding pistache

---
+++ Project/Libs/Pistache/Pistache.py
@@ -0,0 +1,8 @@
+# Source: https://github.com/NearlyTRex/pistache
+# Version: Gitmaster
+
+# Library info
+Setup = {}
+Setup['url'] = "https://github.com/NearlyTRex/pistache/archive/master.zip"
+Setup['extractdir'] = "pistache-master"
+Setup['steps'] = {}
