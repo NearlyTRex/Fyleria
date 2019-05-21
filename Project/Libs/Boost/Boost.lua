6cb84810337993e01d3f1afac3c1de053027e644
NearlyTRex <evermoresecrets@gmail.com>
2019-05-20 23:35:09 -0400

    Removing boost thread sources

--- Project/Libs/Boost/Boost.lua
+++ Project/Libs/Boost/Boost.lua
@@ -32,15 +32,9 @@ libBoost_sources = {
     libBoost_srcdir .. "program_options/src/utf8_codecvt_facet.cpp",
     libBoost_srcdir .. "program_options/src/value_semantic.cpp",
     libBoost_srcdir .. "program_options/src/variables_map.cpp",
-    libBoost_srcdir .. "thread/src/future.cpp",
-    libBoost_srcdir .. "thread/src/tss_null.cpp",
     libBoost_srcdir .. "system/src/error_code.cpp"
 }
 if os.host() == "windows" then
     table.insert(libBoost_sources, libBoost_srcdir .. "filesystem/src/windows_file_codecvt.cpp")
     table.insert(libBoost_sources, libBoost_srcdir .. "program_options/src/winmain.cpp")
-    table.insert(libBoost_sources, libBoost_srcdir .. "thread/src/win32/thread_primitives.cpp")
-else
-    table.insert(libBoost_sources, libBoost_srcdir .. "thread/src/pthread/once.cpp")
-    table.insert(libBoost_sources, libBoost_srcdir .. "thread/src/pthread/thread.cpp")
 end
