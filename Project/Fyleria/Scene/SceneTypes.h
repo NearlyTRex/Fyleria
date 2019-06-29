e4b556aa97162125f47c2734bd74014e98ae95d5
NearlyTRex <evermoresecrets@gmail.com>
2019-06-29 14:25:36 -0400

    Adding scene types

---
+++ Project/Fyleria/Scene/SceneTypes.h
@@ -0,0 +1,23 @@
+// Fyleria Engine
+// Copyright © 2019 Go Go Gecko Productions
+
+#ifndef _GECKO_SCENE_TYPES_H_
+#define _GECKO_SCENE_TYPES_H_
+
+// Internal includes
+#include "Utility/Assert.h"
+#include "Utility/Macros.h"
+#include "Utility/Enum.h"
+
+namespace Gecko
+{
+
+BETTER_ENUM(SceneMessageFunctionType, Int,
+    None,
+    SwitchScene,
+    ReloadHtml
+);
+
+};
+
+#endif
