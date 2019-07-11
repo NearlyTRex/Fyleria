359efe7eff4b628dafe83da4cab25da30bd6ca96
NearlyTRex <evermoresecrets@gmail.com>
2019-07-10 22:30:46 -0400

    Some spacing cleanup

--- Project/Fyleria/Utility/Python.h
+++ Project/Fyleria/Utility/Python.h
@@ -16,36 +16,36 @@
 #include "Utility/Types.h"
 
 // PyBind defines
-#define PyBindInitializeInterpreter         pybind11::initialize_interpreter
-#define PyBindFinalizeInterpreter           pybind11::finalize_interpreter
-#define PyBindAcquireScopedGIL              pybind11::gil_scoped_acquire
-#define PyBindReleaseScopedGIL              pybind11::gil_scoped_release
-#define PyBindRuntimeException              pybind11::error_already_set
-#define PyBindExec                          pybind11::exec
-#define PyBindEval                          pybind11::eval
-#define PyBindEvalFile                      pybind11::eval_file
-#define PyBindModuleImport                  pybind11::module::import
-#define PyBindCast                          pybind11::cast
-#define PyBindObject                        pybind11::object
-#define PyBindHandle                        pybind11::handle
-#define PyBindModule                        pybind11::module
-#define PyBindObject                        pybind11::object
-#define PyBindClass                         pybind11::class_
-#define PyBindConst                         pybind11::const_
-#define PyBindEnum                          pybind11::enum_
-#define PyBindInit                          pybind11::init
-#define PyBindArg                           pybind11::arg
-#define PyBindVector                        pybind11::bind_vector
-#define PyBindMap                           pybind11::bind_map
-#define PyBindOverloadCast                  pybind11::overload_cast
-#define PyBindReturnTakeOwnership           pybind11::return_value_policy::take_ownership
-#define PyBindReturnCopy                    pybind11::return_value_policy::copy
-#define PyBindReturnMove                    pybind11::return_value_policy::move
-#define PyBindReturnRef                     pybind11::return_value_policy::reference
-#define PyBindReturnRefInternal             pybind11::return_value_policy::reference_internal
-#define PyBindReturnAuto                    pybind11::return_value_policy::automatic
-#define PyBindReturnAutoRef                 pybind11::return_value_policy::automatic_reference
-#define PyBindMakeImplicitlyConvertible     pybind11::implicitly_convertible
+#define PyBindInitializeInterpreter pybind11::initialize_interpreter
+#define PyBindFinalizeInterpreter pybind11::finalize_interpreter
+#define PyBindAcquireScopedGIL pybind11::gil_scoped_acquire
+#define PyBindReleaseScopedGIL pybind11::gil_scoped_release
+#define PyBindRuntimeException pybind11::error_already_set
+#define PyBindExec pybind11::exec
+#define PyBindEval pybind11::eval
+#define PyBindEvalFile pybind11::eval_file
+#define PyBindModuleImport pybind11::module::import
+#define PyBindCast pybind11::cast
+#define PyBindObject pybind11::object
+#define PyBindHandle pybind11::handle
+#define PyBindModule pybind11::module
+#define PyBindObject pybind11::object
+#define PyBindClass pybind11::class_
+#define PyBindConst pybind11::const_
+#define PyBindEnum pybind11::enum_
+#define PyBindInit pybind11::init
+#define PyBindArg pybind11::arg
+#define PyBindVector pybind11::bind_vector
+#define PyBindMap pybind11::bind_map
+#define PyBindOverloadCast pybind11::overload_cast
+#define PyBindReturnTakeOwnership pybind11::return_value_policy::take_ownership
+#define PyBindReturnCopy pybind11::return_value_policy::copy
+#define PyBindReturnMove pybind11::return_value_policy::move
+#define PyBindReturnRef pybind11::return_value_policy::reference
+#define PyBindReturnRefInternal pybind11::return_value_policy::reference_internal
+#define PyBindReturnAuto pybind11::return_value_policy::automatic
+#define PyBindReturnAutoRef pybind11::return_value_policy::automatic_reference
+#define PyBindMakeImplicitlyConvertible pybind11::implicitly_convertible
 
 namespace Gecko
 {
