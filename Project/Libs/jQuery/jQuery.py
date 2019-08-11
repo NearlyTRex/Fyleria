5175f5b91f7421721cbe02605b98ca2da322d257
NearlyTRex <evermoresecrets@gmail.com>
2019-08-11 15:22:37 -0400

    Updating jquery

--- Project/Libs/jQuery/jQuery.py
+++ Project/Libs/jQuery/jQuery.py
@@ -2,8 +2,8 @@
 # Version: Gitmaster
 
 # Steps
-CopyFileBootstrapJs = 'CopyFile("dist/jquery.min.js", "$(RootPath)/Data/Libs/jquery.min.js")'
-CopyFileBootstrapSlimJs = 'CopyFile("dist/jquery.slim.min.js", "$(RootPath)/Data/Libs/jquery.slim.min.js")'
+CopyFileJQueryJs = 'CopyFile("dist/jquery.min.js", "$(RootPath)/Data/Libs/jquery.min.js")'
+CopyFileJQuerySlimJs = 'CopyFile("dist/jquery.slim.min.js", "$(RootPath)/Data/Libs/jquery.slim.min.js")'
 
 # Library info
 Setup = {}
@@ -11,5 +11,5 @@ Setup['url'] = "https://github.com/NearlyTRex/jquery-dist/archive/master.zip"
 Setup['extractdir'] = "jquery-dist-master"
 Setup['steps'] = {}
 Setup['steps']['all'] = []
-Setup['steps']['all'].append(CopyFileBootstrapJs)
-Setup['steps']['all'].append(CopyFileBootstrapSlimJs)
+Setup['steps']['all'].append(CopyFileJQueryJs)
+Setup['steps']['all'].append(CopyFileJQuerySlimJs)
