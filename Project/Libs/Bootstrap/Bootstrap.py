d59e1247525ca980da5657c4ce9fbe0a6496dc26
NearlyTRex <evermoresecrets@gmail.com>
2019-08-11 22:30:17 -0400

    Adding map files to be copied

--- Project/Libs/Bootstrap/Bootstrap.py
+++ Project/Libs/Bootstrap/Bootstrap.py
@@ -3,9 +3,13 @@
 
 # Steps
 CopyFileBootstrapCss = 'CopyFile("dist/css/bootstrap.min.css", "$(RootPath)/Data/Libs/bootstrap.min.css")'
+CopyFileBootstrapCssMap = 'CopyFile("dist/css/bootstrap.min.css.map", "$(RootPath)/Data/Libs/bootstrap.min.css.map")'
 CopyFileBootstrapGridCss = 'CopyFile("dist/css/bootstrap-grid.min.css", "$(RootPath)/Data/Libs/bootstrap-grid.min.css")'
+CopyFileBootstrapGridCssMap = 'CopyFile("dist/css/bootstrap-grid.min.css.map", "$(RootPath)/Data/Libs/bootstrap-grid.min.css.map")'
 CopyFileBootstrapRebootCss = 'CopyFile("dist/css/bootstrap-reboot.min.css", "$(RootPath)/Data/Libs/bootstrap-reboot.min.css")'
+CopyFileBootstrapRebootCssMap = 'CopyFile("dist/css/bootstrap-reboot.min.css.map", "$(RootPath)/Data/Libs/bootstrap-reboot.min.css.map")'
 CopyFileBootstrapJs = 'CopyFile("dist/js/bootstrap.min.js", "$(RootPath)/Data/Libs/bootstrap.min.js")'
+CopyFileBootstrapJsMap = 'CopyFile("dist/js/bootstrap.min.js.map", "$(RootPath)/Data/Libs/bootstrap.min.js.map")'
 
 # Library info
 Setup = {}
@@ -14,6 +18,10 @@ Setup['extractdir'] = "bootstrap-master"
 Setup['steps'] = {}
 Setup['steps']['all'] = []
 Setup['steps']['all'].append(CopyFileBootstrapCss)
+Setup['steps']['all'].append(CopyFileBootstrapCssMap)
 Setup['steps']['all'].append(CopyFileBootstrapGridCss)
+Setup['steps']['all'].append(CopyFileBootstrapGridCssMap)
 Setup['steps']['all'].append(CopyFileBootstrapRebootCss)
+Setup['steps']['all'].append(CopyFileBootstrapRebootCssMap)
 Setup['steps']['all'].append(CopyFileBootstrapJs)
+Setup['steps']['all'].append(CopyFileBootstrapJsMap)
--- Project/Libs/jQueryTerminal/jQueryTerminal.py
+++ Project/Libs/jQueryTerminal/jQueryTerminal.py
@@ -3,7 +3,9 @@
 
 # Steps
 CopyFileJQueryTerminalCss = 'CopyFile("css/jquery.terminal.min.css", "$(RootPath)/Data/Libs/jquery.terminal.min.css")'
+CopyFileJQueryTerminalCssMap = 'CopyFile("css/jquery.terminal.min.css.map", "$(RootPath)/Data/Libs/jquery.terminal.min.css.map")'
 CopyFileJQueryTerminalJs = 'CopyFile("js/jquery.terminal.min.js", "$(RootPath)/Data/Libs/jquery.terminal.min.js")'
+CopyFileJQueryTerminalJsMap = 'CopyFile("js/jquery.terminal.min.js.map", "$(RootPath)/Data/Libs/jquery.terminal.min.js.map")'
 CopyFileJQueryMousewheelJs = 'CopyFile("js/jquery.mousewheel-min.js", "$(RootPath)/Data/Libs/jquery.mousewheel.min.js")'
 
 # Library info
@@ -13,5 +15,7 @@ Setup['extractdir'] = "jquery.terminal-master"
 Setup['steps'] = {}
 Setup['steps']['all'] = []
 Setup['steps']['all'].append(CopyFileJQueryTerminalCss)
+Setup['steps']['all'].append(CopyFileJQueryTerminalCssMap)
 Setup['steps']['all'].append(CopyFileJQueryTerminalJs)
+Setup['steps']['all'].append(CopyFileJQueryTerminalJsMap)
 Setup['steps']['all'].append(CopyFileJQueryMousewheelJs)
