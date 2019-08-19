d58955c675520c6637ac9c849da47b4a68702640
NearlyTRex <evermoresecrets@gmail.com>
2019-08-19 08:32:38 -0400

    Adding memcache wrapper

---
+++ Project/Fyleria/Utility/MemCache.h
@@ -0,0 +1,14 @@
+// Fyleria Engine
+// Copyright © 2019 Go Go Gecko Productions
+
+#ifndef _GECKO_MEMCACHE_H_
+#define _GECKO_MEMCACHE_H_
+
+// External includes
+#include <lru/lru.hpp>
+
+// Cache types
+#define MemCacheBasicType LRU::Cache
+#define MemCacheTimedType LRU::TimedCache
+
+#endif
