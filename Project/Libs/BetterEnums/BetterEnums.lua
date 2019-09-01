83790f5b8bda90cd35340c3f4890e7675a5f435a
NearlyTRex <evermoresecrets@gmail.com>
2019-09-01 09:42:50 -0400

    Fixing define

--- Project/Libs/BetterEnums/BetterEnums.lua
+++ Project/Libs/BetterEnums/BetterEnums.lua
@@ -12,7 +12,7 @@ libBetterEnums_includedirs = {
 
 -- Defines
 libBetterEnums_defines = {
-    "BETTER_ENUMS_MACRO_FILE \"enum_macros.h\""
+    "BETTER_ENUMS_MACRO_FILE=\"enum_macros.h\""
 }
 libBetterEnums_debugdefines = {
 }
