4535dec3d889ee7eba2eb2c6ed4e4e24b1db5ce0
Pamela Sera <NearlyTRex@users.noreply.github.com>
2019-09-23 23:57:23 -0400

    Switching to gtk 3.24

--- Project/Libs/Gtk/Gtk.py
+++ Project/Libs/Gtk/Gtk.py
@@ -1,8 +1,8 @@
-# Source: https://github.com/NearlyTRex/gtk
+# Source: https://github.com/NearlyTRex/gtk/tree/gtk-3-24
 # Version: Gitmaster
 
 # Library info
 Setup = {}
-Setup['url'] = "https://github.com/NearlyTRex/gtk/archive/master.zip"
-Setup['extractdir'] = "gtk-master"
+Setup['url'] = "https://github.com/NearlyTRex/gtk/archive/gtk-3-24.zip"
+Setup['extractdir'] = "gtk-gtk-3-24"
 Setup['steps'] = {}
