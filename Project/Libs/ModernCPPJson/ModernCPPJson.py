d0594e5c3253b3c21cc2ce2828b10e53a2306834
NearlyTRex <NearlyTRex@users.noreply.github.com>
2019-11-10 14:22:30 -0500

    Updating json script

--- Project/Libs/ModernCPPJson/ModernCPPJson.py
+++ Project/Libs/ModernCPPJson/ModernCPPJson.py
@@ -3,6 +3,6 @@
 
 # Library info
 Setup = {}
-Setup['url'] = "https://github.com/NearlyTRex/Json/archive/Json-3.7.0.zip
+Setup['url'] = "https://github.com/NearlyTRex/Json/archive/Json-3.7.0.zip"
 Setup['extractdir'] = "Json-Json-3.7.0"
 Setup['steps'] = {}
