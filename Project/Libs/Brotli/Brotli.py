4f036315c7f99e4eedea98fd3f37bfa7d6058cde
NearlyTRex <NearlyTRex@users.noreply.github.com>
2019-11-10 14:22:14 -0500

    Updating brotli script

--- Project/Libs/Brotli/Brotli.py
+++ Project/Libs/Brotli/Brotli.py
@@ -1,8 +1,8 @@
-# Source: https://github.com/NearlyTRex/brotli
-# Version: Gitmaster
+# Source: https://github.com/NearlyTRex/Brotli
+# Version: 1.0.7
 
 # Library info
 Setup = {}
-Setup['url'] = "https://github.com/NearlyTRex/brotli/archive/master.zip"
-Setup['extractdir'] = "brotli-master"
+Setup['url'] = "https://github.com/NearlyTRex/Brotli/archive/Brotli-1.0.7.zip"
+Setup['extractdir'] = "Brotli-Brotli-1.0.7"
 Setup['steps'] = {}
