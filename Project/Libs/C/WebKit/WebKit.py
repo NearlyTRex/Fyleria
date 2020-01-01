9a9a6ab092b936ac53a634dfe6dbde0736d4bb78
Pamela Sera <NearlyTRex@users.noreply.github.com>
2020-01-01 17:23:29 -0500

    Adding WebKit and WebKitCairo download scripts

---
+++ Project/Libs/C/WebKit/WebKit.py
@@ -0,0 +1,9 @@
+# Source: https://github.com/NearlyTRex/WebKit
+# Version: 12272019
+
+# Library info
+Setup = {}
+Setup['url'] = "https://github.com/NearlyTRex/WebKit/archive/WebKit-12272019.zip"
+Setup['extractdir'] = "WebKit-WebKit-12272019"
+Setup['steps'] = {}
+Setup['steps']['all'] = []
