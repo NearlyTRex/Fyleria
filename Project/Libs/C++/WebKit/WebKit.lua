110c26b84e534567cb54f526e139200fb97c974e
Pamela Sera <NearlyTRex@users.noreply.github.com>
2020-01-12 23:43:34 -0500

    Updating webkit build files

---
+++ Project/Libs/C++/WebKit/WebKit.lua
@@ -0,0 +1,41 @@
+-- Configuration
+libWebKit_extradir = "Libs/C++/WebKit/extra/"
+libWebKit_origdir = "Libs/C++/WebKit/orig/"
+libWebKit_srcdir = libWebKit_origdir
+libWebKit_incdir = libWebKit_origdir
+
+-- Includes
+libWebKit_includedirs = {
+}
+if os.host() == "windows" then
+    table.insert(libWebKit_includedirs, libWebKit_incdir .. "WebKitBuild/Release/ANGLE/Headers")
+    table.insert(libWebKit_includedirs, libWebKit_incdir .. "WebKitBuild/Release/PAL/Headers")
+    table.insert(libWebKit_includedirs, libWebKit_incdir .. "WebKitBuild/Release/WTF/Headers")
+    table.insert(libWebKit_includedirs, libWebKit_incdir .. "WebKitBuild/Release/JavaScriptCore/Headers")
+    table.insert(libWebKit_includedirs, libWebKit_incdir .. "WebKitBuild/Release/WebKitLegacy/Headers")
+	table.insert(libWebKit_includedirs, libWebKit_incdir .. "WebKitBuild/Release/WebKit/Headers")
+end
+
+-- Defines
+libWebKit_defines = {
+}
+libWebKit_debugdefines = {
+}
+libWebKit_releasedefines = {
+}
+
+-- Build options
+libWebKit_buildoptions = {
+}
+
+-- Link options
+libWebKit_linkoptions = {
+}
+
+-- Libs
+libWebKit_libs = {
+}
+
+-- Sources
+libWebKit_sources = {
+}
--- Project/Libs/C++/WebKit/WebKit.py
+++ Project/Libs/C++/WebKit/WebKit.py
@@ -1,9 +1,20 @@
 # Source: https://github.com/NearlyTRex/WebKit
-# Version: 12272019
+# Version: r254199
+
+# Steps
+WebKitCairo_Build = 'RunProcess("perl Tools/Scripts/build-webkit --wincairo --release")'
+WebKitGTK_InstallDeps = 'RunProcess("perl Tools/gtk/install-dependencies")'
+WebKitGTK_UpdateLibs = 'RunProcess("perl Tools/Scripts/update-webkitgtk-libs")'
+WebKitGTK_Build = 'RunProcess("perl Tools/Scripts/build-webkit --gtk --release")'
 
 # Library info
 Setup = {}
-Setup['url'] = "https://github.com/NearlyTRex/WebKit/archive/WebKit-12272019.zip"
-Setup['extractdir'] = "WebKit-WebKit-12272019"
+Setup['url'] = "https://github.com/NearlyTRex/WebKit/archive/WebKit-r254199.zip"
+Setup['extractdir'] = "WebKit-WebKit-r254199"
 Setup['steps'] = {}
-Setup['steps']['all'] = []
+Setup['steps']['windows'] = []
+Setup['steps']['windows'].append(WebKitCairo_Build)
+Setup['steps']['linux'] = []
+Setup['steps']['linux'].append(WebKitGTK_InstallDeps)
+Setup['steps']['linux'].append(WebKitGTK_UpdateLibs)
+Setup['steps']['linux'].append(WebKitGTK_Build)
