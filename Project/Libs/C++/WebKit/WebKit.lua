601a6f1ef910234bb8f5f162ffa9bd66f060ed94
Pamela Sera <NearlyTRex@users.noreply.github.com>
2020-01-20 00:43:48 -0500

    Updating windows libs locations

--- Project/Libs/C++/WebKit/WebKit.lua
+++ Project/Libs/C++/WebKit/WebKit.lua
@@ -4,7 +4,7 @@ libWebKit_origdir = "Libs/C++/WebKit/orig/"
 libWebKit_srcdir = libWebKit_origdir
 libWebKit_incdir = libWebKit_origdir
 
--- Includes
+-- Include dirs
 libWebKit_includedirs = {
 }
 if os.host() == "windows" then
@@ -14,6 +14,13 @@ if os.host() == "windows" then
     table.insert(libWebKit_includedirs, libWebKit_incdir .. "WebKitBuild/Release/JavaScriptCore/Headers")
     table.insert(libWebKit_includedirs, libWebKit_incdir .. "WebKitBuild/Release/WebKitLegacy/Headers")
 	table.insert(libWebKit_includedirs, libWebKit_incdir .. "WebKitBuild/Release/WebKit/Headers")
+end
+
+-- Lib dirs
+libWebKit_libdirs = {
+}
+if os.host() == "windows" then
+    table.insert(libWebKit_libdirs, libWebKit_origdir .. "WebKitBuild/Release/lib64")
 end
 
 -- Defines
@@ -35,6 +42,13 @@ libWebKit_linkoptions = {
 -- Libs
 libWebKit_libs = {
 }
+if os.host() == "windows" then
+    table.insert(libWebKit_libs, "ANGLE.lib")
+    table.insert(libWebKit_libs, "PAL.lib")
+    table.insert(libWebKit_libs, "WTF.lib")
+    table.insert(libWebKit_libs, "JavaScriptCore.lib")
+    table.insert(libWebKit_libs, "WebKit2.lib")
+end
 
 -- Sources
 libWebKit_sources = {
