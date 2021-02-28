17650f5777962d4d0475dc673511ca1392fdd3bf
NearlyTRex <evermoresecrets@gmail.com>
2021-02-28 11:22:54 -0500

    Adding build step and removing slim reference

--- Project/Libs/JavaScript/jQuery/jQuery.py
+++ Project/Libs/JavaScript/jQuery/jQuery.py
@@ -1,12 +1,12 @@
 # Source: https://github.com/NearlyTRex/jQuery
 
 # Steps
+Build = 'RunProcess("npm run build")'
 CopyFileJQueryJs = 'CopyFile("dist/jquery.min.js", "$(RootPath)/Data/Libs/jquery.min.js")'
-CopyFileJQuerySlimJs = 'CopyFile("dist/jquery.slim.min.js", "$(RootPath)/Data/Libs/jquery.slim.min.js")'
 
 # Library info
 Setup = {}
 Setup['steps'] = {}
 Setup['steps']['all'] = []
+Setup['steps']['all'].append(Build)
 Setup['steps']['all'].append(CopyFileJQueryJs)
-Setup['steps']['all'].append(CopyFileJQuerySlimJs)
